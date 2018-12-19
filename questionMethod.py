e9c1715c7cc6a41261a9ae9611f1b74be4ba7e3f
kagehina919 <ns19041997@gmail.com>
2018-12-20 00:02:54 +0530

    Potion Finder

--- questionMethod.py
+++ questionMethod.py
@@ -3,3 +3,13 @@ t = input()
 t = list(map(int, t.split()))
 print(t[0]+t[2]+t[4]+t[6]+t[8])
 print(t[1]+t[3]+t[5]+t[7])
+
+# potion finder
+a =input()
+sum = 0;
+a =list(a)
+for i in range(len(a)):
+    sum += (int(a[i]) * (i+1))
+if (sum%11)==0:
+    print("Legal ISBN") 
+else:print("Illegal ISBN")

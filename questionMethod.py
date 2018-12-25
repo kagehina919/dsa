beb33f43640b47d7e7270235ab609c6cca4da5a0
kagehina919 <ns19041997@gmail.com>
2018-12-25 17:46:40 +0530

    Insertion sort 1

--- questionMethod.py
+++ questionMethod.py
@@ -271,3 +271,25 @@ def marsExploration(s):
             else:continue
         print(count)
 marsExploration(s)
+
+# insertionsort1
+import sys
+
+def insertionSort1(n, arr):
+    a = arr[-1]
+    i = n-2
+    
+    while (a < arr[i]) and (i >= 0):
+        arr[i+1] = arr[i]
+        print(' '.join(map(str, arr)))
+        i -= 1
+        
+    arr[i+1] = a
+    print(' '.join(map(str, arr)))
+    
+if __name__ == "__main__":
+    n = int(input().strip())
+    arr = list(map(int, input().strip().split(' ')))
+    insertionSort1(n, arr)
+
+

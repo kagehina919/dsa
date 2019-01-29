50cb7afdac641034bd8e6afdb913a638e794ed0e
kagehina919 <ns19041997@gmail.com>
2019-01-29 18:46:50 +0530

    Array Sum

--- questionMethod.py
+++ questionMethod.py
@@ -334,3 +334,31 @@ def marsExploration(s):
             else:continue
         print(count)
 marsExploration(s)
+
+# Array Sum
+#!/bin/python3
+
+import os
+import sys
+
+#
+# Complete the simpleArraySum function below.
+#
+def simpleArraySum(ar):
+    #
+    # Write your code here.
+    #
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = simpleArraySum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+

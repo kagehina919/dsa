ad43ff6bf97f578de0c106037330f00c84565042
kagehina919 <ns19041997@gmail.com>
2019-05-20 14:50:55 +0530

    Max Depth of tree

---
+++ cpp/MaxDepth.cpp
@@ -0,0 +1,19 @@
+// https://www.interviewbit.com/problems/max-depth-of-binary-tree/
+
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+int Solution::maxDepth(TreeNode* A) {
+    if(A == NULL) {
+        return 0;
+    }
+    int lcount = maxDepth(A->left);
+    int rcount = maxDepth(A->right);
+    return max(lcount, rcount) + 1;
+}

58630f7da078f358cae1f3c77bce5abd1b9abb23
kagehina919 <ns19041997@gmail.com>
2019-06-25 13:47:05 +0530

    Pythagorean triplet 2

--- cpp/pythagorean.cpp
+++ cpp/pythagorean.cpp
@@ -18,3 +18,25 @@ bool main() {
     }
     return false;
 }
+
+// O(n2) solution
+
+int pythagoras() {
+    int arr[] = [3,4,5,12,9,13];
+    for(int i=0;i<n;i++) {
+        arr[i] = arr[i]*arr[i];
+    }
+
+    sort(arr, arr+n);
+
+    for (int i = n - 1; i >= 2; i--) {
+        int l = 0;
+        int r = i - 1; 
+        while (l < r) {
+            if (arr[l] + arr[r] == arr[i]) 
+                return true;  
+            (arr[l] + arr[r] < arr[i]) ? l++ : r--; 
+        } 
+    }  
+    return false; 
+}

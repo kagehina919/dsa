9468ed0f8bfe9c475090f9e90b6620bfd9a5cc8c
kagehina919 <ns19041997@gmail.com>
2019-06-25 12:24:01 +0530

    kth smallest element

---
+++ cpp/ksmallest.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// nlogn solution
+int main() {
+    int arr[] = [1,2,3,2,4,0,1,5];
+    int n = len(arr);
+    sort(arr, arr+n);
+    cout << arr[k-1];
+    return 0;
+}

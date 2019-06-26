2d1fa2661a909dd75e75e8ee6934edbd33e18cfc
kagehina919 <ns19041997@gmail.com>
2019-06-26 14:43:26 +0530

    Max subarray of length 0

---
+++ python/hashing/subarraySumZero.py
@@ -0,0 +1,20 @@
+import operator
+
+arr[] = [-1, -3, 3, 0, 1]
+dict = {}
+
+sum = 0
+leng = 0
+n = len(arr)
+for i in range(0, n):
+    sum += arr[i]
+    if arr[i] is 0 and leng = 0:
+        leng +=1
+    if sum = 0:
+        leng += 1
+    if sum in dict:
+        leng = max(leng, i-dict[sum])
+    else:
+        dict[sum] = i
+
+print(leng)

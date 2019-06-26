5cd681f22f56c777bc84f4453603a39c6e2eb769
kagehina919 <ns19041997@gmail.com>
2019-06-26 15:21:29 +0530

    four sum 2

--- python/hashing/fourSum.py
+++ python/hashing/fourSum.py
@@ -2,15 +2,10 @@ a = [10, 2, 3, 4, 5, 9, 7, 8]
 k = 23
 dict = {}
 
-for i in range(0, 5):
-    for j in range(1, 6):
-        for k in range(2, 7):
-            for l in range(3, 8):
-                sum = a[i] + a[j] + a[k] + a[l]
-                if sum is k:
-                    if (a[i], a[j], a[k], a[l]) not in dict:
-                        dict[(a[i], a[j], a[k], a[l])] = 1
-
-for key in dict:
-    print(key)
-
+for i in range(0, 8):
+    for j in range(1, 8):
+        sum = a[i] + a[j]
+        if sum not in dict:
+            dict[sum] = 1
+    if (k-sum) in dict:
+        print("YES")

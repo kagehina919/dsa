0c2bca97281b8fec94e8f9e36ac5e0cc52cee93b
kagehina919 <ns19041997@gmail.com>
2019-07-05 18:49:07 +0530

    Minimum chars to add to make palindrome

---
+++ python/palindrome.py
@@ -0,0 +1,16 @@
+# Main idea is to make a hash map of the characters in the string,
+# or count the individual distinct chars in the string.
+# We will have to add n-1 characters to the string to get the desired palindrome,
+# where n is the current number of distinct chars in the string.
+
+def main():
+    a = input()
+    n = len(a)
+    dict = {}
+    for i in range(n):
+        if a[i] in dict:
+            dict(a[i]) += 1
+        else:
+            dict(a[i]) = 1
+    b = len(dict)
+    print(b-1)

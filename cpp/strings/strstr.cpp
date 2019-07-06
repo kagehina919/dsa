562723bfa9ffe0cb18c4964a32b20c93aa222e9e
kagehina919 <ns19041997@gmail.com>
2019-07-06 12:39:44 +0530

    strstr in a string

---
+++ cpp/strings/strstr.cpp
@@ -0,0 +1,29 @@
+/* The task here is to find the first occurrence of a string in another.
+   The strstr() function in c++ does the same job.
+   Here, use of 2 pointer technique can get the solution in O(n),
+   a simple solution of running 2 loops is also possible with a complexity of O(n^2) */
+
+// 2 loops solution
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    string s;
+    cin >> s;
+    string x;
+    cin >> x;
+    int sum = 0;
+    int n = len(s);
+    int m = len(x);
+    if(m>n) cout << -1 << endl;
+    else {
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+                if(s[i] == x[j]) {
+                    sum += 1;
+                }
+            if(sum == m) return i+1;
+            }
+        }
+        return -1;
+    }
+}

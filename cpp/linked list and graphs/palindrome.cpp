2cdd980bb2510879f69a4e6cccf90334633e15eb
kagehina919 <ns19041997@gmail.com>
2019-07-06 19:17:12 +0530

    Palindrome linked list

---
+++ cpp/linked list and graphs/palindrome.cpp
@@ -0,0 +1,74 @@
+{
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <stack>
+using namespace std;
+/* Link list Node */
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+void append(struct Node** head_ref, struct Node **tail_ref, int new_data)
+{
+    struct Node* new_node = new Node(new_data);
+    
+    if (*head_ref == NULL)
+       *head_ref = new_node;
+    else
+       (*tail_ref)->next = new_node;
+    *tail_ref = new_node;
+}
+bool isPalindrome(Node *head);
+/* Driver program to test above function*/
+int main()
+{
+  int T,i,n,l;
+    cin>>T;
+    while(T--){
+    struct Node *head = NULL,  *tail = NULL;
+        cin>>n;
+        for(i=1;i<=n;i++)
+        {
+            cin>>l;
+            append(&head, &tail, l);
+        }
+   	cout<<isPalindrome(head)<<endl;
+    }
+    return 0;
+}
+
+}
+/*This is a function problem.You only need to complete the function given below*/
+
+/*
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+/*You are required to complete this method */
+#include <bits/stdc++.h>
+bool isPalindrome(Node *head) {
+    int count = 0;
+    bool flag = true;
+    vector<int> v;
+    while(head != NULL) {
+        v.push_back(head->data);
+        count += 1;
+        head = head->next;
+    }
+    for(int i=0;i<count;i++) {
+        if(v[i] != v[count-i-1]) flag = false;
+    }
+    if(flag == false) return false;
+    else return true;
+}

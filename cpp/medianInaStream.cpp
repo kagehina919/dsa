273fbfacc55a0af2e968972453b3a15792235ab4
kagehina919 <ns19041997@gmail.com>
2019-07-10 20:17:23 +0530

    HeapSort

--- cpp/medianInaStream.cpp
+++ cpp/medianInaStream.cpp
@@ -27,3 +27,76 @@ int main() {
     }
 	return 0;
 }
+
+// heap solution
+#include<bits/stdc++.h> 
+using namespace std;  
+void printMedians(double arr[], int n) {
+    // max heap
+    priority_queue<double> s; 
+    // min heap
+    priority_queue<double,vector<double>,greater<double>> g; 
+  
+    double med = arr[0]; 
+    s.push(arr[0]); 
+  
+    cout << med << endl;
+    
+    for (int i=1; i < n; i++) 
+    { 
+        double x = arr[i]; 
+        if (s.size() > g.size()) 
+        { 
+            if (x < med) 
+            { 
+                g.push(s.top()); 
+                s.pop(); 
+                s.push(x); 
+            } 
+            else
+                g.push(x); 
+  
+            med = (s.top() + g.top())/2.0; 
+        } 
+        else if (s.size()==g.size()) 
+        { 
+            if (x < med) 
+            { 
+                s.push(x); 
+                med = (double)s.top(); 
+            } 
+            else
+            { 
+                g.push(x); 
+                med = (double)g.top(); 
+            } 
+        } 
+        else
+        { 
+            if (x > med) 
+            { 
+                s.push(g.top()); 
+                g.pop(); 
+                g.push(x); 
+            } 
+            else
+                s.push(x); 
+  
+            med = (s.top() + g.top())/2.0; 
+        } 
+  
+        cout << int(med) << endl; 
+    } 
+}
+
+int main() { 
+    int n;
+    cin >> n;
+    double a[n];
+    for(int i=0;i<n;i++) {
+        int x;
+        cin >> a[i];
+    }
+    printMedians(a, n);
+    return 0; 
+}

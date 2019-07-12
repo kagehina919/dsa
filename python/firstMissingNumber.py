542291d0adaaa1e5ef0c3bc6f2dbc4bd8d7fd6a6
kagehina919 <ns19041997@gmail.com>
2019-07-12 21:02:13 +0530

    First Missing Number O(n) solution

--- python/firstMissingNumber.py
+++ python/firstMissingNumber.py
@@ -11,3 +11,19 @@ class Solution:
                 if(i not in A):
                     return i
             return A[len(A)-1] + 1
+
+# Another, more efficient solution O(n)
+class Solution:
+    # @param A : list of integers
+    # @return an integer
+    def firstMissingPositive(self, A):
+        dict = {}
+        if len(A) == 1 and A[0]<0:
+            return 1
+        for i in range(0, len(A)):
+            if A[i] not in dict:
+                dict[A[i]] = 1
+        for i in range(1, len(A)):
+            if i not in dict:
+                return i
+        return len(A)+1

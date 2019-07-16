2fa7dffafca0dc7b5aab99d9bebda520e8414c3e
kagehina919 <ns19041997@gmail.com>
2019-07-16 19:35:48 +0530

    DP Ugly Numbers working for larger N

--- cpp/Dynamic Programming/UglyNumbers.cpp
+++ cpp/Dynamic Programming/UglyNumbers.cpp
@@ -4,35 +4,35 @@ using namespace std;
 int main() {
 	int n;
 	cin >> n;
+	long long int ugly[10000];
+    long long int next_multiple_of_2 = 2;
+    long long int next_multiple_of_3 = 3;
+    long long int next_multiple_of_5 = 5;
+    int i2 = 0;
+    int i3 = 0;
+    int i5 = 0;
+    ugly[0] = 1;
+    long long int nextUglyNumber=1;
+    for(int i=1;i<10000;i++) {
+        nextUglyNumber = min(next_multiple_of_2, min(next_multiple_of_3, next_multiple_of_5));
+        ugly[i] = nextUglyNumber;
+        if(ugly[i] == next_multiple_of_2) {
+            i2 = i2+1;
+            next_multiple_of_2 = ugly[i2]*2;
+        }
+        if(ugly[i] == next_multiple_of_3) {
+            i3 = i3+1;
+            next_multiple_of_3 = ugly[i3]*3;
+        }
+        if(ugly[i] == next_multiple_of_5) {
+            i5 = i5+1;
+            next_multiple_of_5 = ugly[i5]*5;
+        }
+    }
 	for(int i=0;i<n;i++) {
 	    int m;
 	    cin >> m;
-	    int ugly[m];
-        int next_multiple_of_2 = 2;
-        int next_multiple_of_3 = 3;
-        int next_multiple_of_5 = 5;
-        int i2 = 0;
-        int i3 = 0;
-        int i5 = 0;
-        ugly[0] = 1;
-        int nextUglyNumber=1;
-        for(int i=1;i<m;i++) {
-            nextUglyNumber = min(next_multiple_of_2, min(next_multiple_of_3, next_multiple_of_5));
-            ugly[i] = nextUglyNumber;
-            if(ugly[i] == next_multiple_of_2) {
-                i2 = i2+1;
-                next_multiple_of_2 = ugly[i2]*2;
-            }
-            if(ugly[i] == next_multiple_of_3) {
-                i3 = i3+1;
-                next_multiple_of_3 = ugly[i3]*3;
-            }
-            if(ugly[i] == next_multiple_of_5) {
-                i5 = i5+1;
-                next_multiple_of_5 = ugly[i5]*5;
-            }
-        }
-        cout<<nextUglyNumber<<endl;
+        cout << ugly[m-1] << endl;
 	}
 	return 0;
 }

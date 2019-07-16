32d71870ed2e1eec8b84dea3cadc466a874d98bc
kagehina919 <ns19041997@gmail.com>
2019-07-16 18:57:13 +0530

    DP fibonacci

--- cpp/Dynamic Programming/Fibonacci.cpp
+++ cpp/Dynamic Programming/Fibonacci.cpp
@@ -10,3 +10,16 @@ int main() {
     cin >> n;
     return fib(n);
 }
+
+// Fibonacci DP implementation
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    arr[0] = 1;
+    arr[1] = 1;
+    for(int i=2;i<=n;i++) {
+        arr[i] = arr[i-1] + arr[i-2];
+    }
+    cout << arr[n] << endl;
+}

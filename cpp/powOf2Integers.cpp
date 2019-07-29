f79b7a2da0c99a100834f8ac59ce8634be2be09c
kagehina919 <ns19041997@gmail.com>
2019-07-29 18:57:00 +0530

    Power of 2 numbers efficient solution

--- cpp/powOf2Integers.cpp
+++ cpp/powOf2Integers.cpp
@@ -7,3 +7,14 @@ int Solution::isPower(int A) {
     }
     return 0;
 }
+
+// Efficient solution
+int Solution::isPower(int A) {
+    if(A==1) return 1;
+    for(int i=2;i<A;i++) {
+        int modified = log(A)/log(i);
+        int reconstructed = pow((double) i, (double) modified);
+        if(reconstructed == A) return 1;
+    }
+    return 0;
+}

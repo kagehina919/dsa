98027b0885c3d2442d82356f6082203c54ad2533
kagehina919 <ns19041997@gmail.com>
2019-07-29 15:45:34 +0530

    Power of 2

---
+++ cpp/strings/powOf2.cpp
@@ -0,0 +1,24 @@
+string mulby2(string s){
+    int n = s.size();
+    int carry = 0;
+    int temp;
+    for(int i=n-1;i>=0;i--){
+        temp = ((s[i] - '0')*2);
+        s[i] = '0'+((temp+carry)%10);
+        carry = (temp+carry)/10;
+    }
+    if(carry)
+    s.insert (0, 1, carry+'0');
+    return s;
+}
+int Solution::power(string A) {
+    string s = "1";
+    while(true){
+        s = mulby2(s);
+        // cout<<s<<endl;
+        if(A==s)
+            return 1;
+        if(s.size()>A.size())
+            return 0;
+    }
+}

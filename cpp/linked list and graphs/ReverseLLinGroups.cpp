a984a0f8ddb01deb0d642a8aa53495c63c704378
kagehina919 <ns19041997@gmail.com>
2019-07-29 16:00:05 +0530

    Reverse linked list in groups

--- cpp/linked list and graphs/ReverseLLinGroups.cpp
+++ cpp/linked list and graphs/ReverseLLinGroups.cpp
@@ -79,16 +79,16 @@ int main(void)
   }
 */
 struct node *reverse (struct node *head, int k) {
-    int count = 0;
     node* current = head;
     node* next = NULL;
     node* prev = NULL;
+    int count = 0;
     while(current != NULL && count<k) {
-        count++;
         next = current->next;
         current->next = prev;
         prev = current;
         current = next;
+        count++;
     }
     if(next != NULL) head->next = reverse(next, k);
     return prev;

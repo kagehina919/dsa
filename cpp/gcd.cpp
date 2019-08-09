aeba2b12ff01ad0800e3e8972fc2062fe104fcdd
kagehina919 <ns19041997@gmail.com>
2019-08-09 13:17:05 +0530

    GCD of 2 numbers

--- cpp/gcd.cpp
+++ cpp/gcd.cpp
@@ -1,12 +1,10 @@
 // https://www.interviewbit.com/problems/greatest-common-divisor/
 
 #include <vector>
-int Solution::gcd(int A, int B) {
-    if (A == 0 && B !=0) return B; 
-    else if (B == 0 && A != 0) return A; 
-    else if (A == B) return A;
-    else if (A == B == 0) return 1;
-    if (A > B) 
-        return gcd(A-B, B); 
-    return gcd(A, B-A);
+int Solution::gcd(int a, int b) {
+    if(a==b) return a;
+    if(a==0) return b;
+    if(b==0) return a;
+    if(a>b) return gcd(a-b, b);
+    else return gcd(a, b-a);
 }
